a9b2c4c2d14e05edc1df31327ccc8310b17116b5
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-11-14 17:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function trailingZeros(n) {
   //your JS code here. If required.
-}
+  n = Number(n);
+  let count = 0;
+
+  while (n > 0) {
+    n = Math.floor(n / 5);
+    count += n;
+  }
+
+  return count;
+} 
 
 const input = prompt("Enter a number");
-alert(trailingZeros(input));
+alert(trailingZeros(input)); 
